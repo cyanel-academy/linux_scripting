287ec9f277ddaa17c7f72aedcae19bde043c7b3d
SARA EL FEGGOURI <sara.el-feggouri.27@eigsi.fr>
2026-03-27 11:14:51 +0100

    Nettoyage : simonshreste en local

--- simon.sh
+++
@@ -1,4 +0,0 @@
-echo -e '#!/bin/bash\n\nnum1=$1\nnum2=$2\nsum=$((num1 + num2))\necho "Add $num1 to $num2 = $sum"' > add.sh && chmod +x add.sh
-git add add.sh
-git commit -m "Ajout du script d'addition pour le TP" 
-git push origin feature/sara-travail
