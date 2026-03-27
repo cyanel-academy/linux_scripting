f79d0b8d4aadd1684a580d7213a1873da2fd3fd8
SARA EL FEGGOURI <sara.el-feggouri.27@eigsi.fr>
2026-03-27 12:56:21 +0100

    Vrai code simon avec Top 10

--- simon.sh
+++ simon.sh
@@ -1,4 +1,4 @@
-echo -e '#!/bin/bash\n\nnum1=$1\nnum2=$2\nsum=$((num1 + num2))\necho "Add $num1 to $num2 = $sum"' > add.sh && chmod +x add.sh
-git add add.sh
-git commit -m "Ajout du script d'addition pour le TP" 
+git add simon.sh
+git commit -m "correction : Vrai code du simon avec  Top 10"
 git push origin feature/sara-travail
+
