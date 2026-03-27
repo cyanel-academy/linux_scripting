11bd0b48e9201e7293cb9b1b30e629489f89f815
marc.barghi <marc.barghi.27@eigsi.fr>
2026-03-27 12:22:33 +0100

    Added comments and a few fixes

--- Scripts & Game/Wordle/play_again.sh
+++ Scripts & Game/Wordle/play_again.sh
@@ -20,6 +20,7 @@ case "$user_input" in
 	rank=1
     	# we get all the fields from the file
 	printf "%-6s %-15s\t%s\n" "RANK" "USERNAME" "SCORE"
+	# we sort the file's content by the number of wins and get the 10 best (first)
 	sort -k2,2nr "$filename" | head -n 10 | while read usr score; do
 		printf "#%-5d %-15s\t%d\n" "$rank" "$usr" "$score"
 		rank=$((rank + 1))
--- Scripts & Game/Wordle/save_score_username.sh
+++ Scripts & Game/Wordle/save_score_username.sh
@@ -7,13 +7,17 @@ filename="scores.txt"
 # if the file doesn't exist, create it
 touch "$filename"
 
-# get the current score of the player
+# get the current score of the player (-P to be able to put \t)
 current_score=$(grep -P "^${username}\t" "$filename" | cut -f2)
 
+# if current_score has a number, the player exists (we change the number of wins), else, it's a new player
 if [ -n "$current_score" ]; then
 	new_score=$((current_score + 1))
 
+	# we directly change the file at the username's line and replace the current with new score
 	sed -i "s/^${username}\t.*/${username}\t${new_score}/" "$filename"
 else
 	printf "%s\t1\n" "$username" >> "$filename"
 fi
+
+exit 0
