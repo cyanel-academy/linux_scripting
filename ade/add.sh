99e39e38fc7f31c4a92b232b2d3ad6048308a00e
iopad-rell <iop.adechian.27@eigsi.fr>
2026-03-27 10:45:34 +0100

    suppression

--- ade/add.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-x=$1
-y=$2
-
-result=$((x + y))
-
-echo "$result"
--- ade/order_from_input.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-read -p "What is the product name? " name #le name est stocké dans la variable $name
-read -p "What is the price? " price #le price est stocké dans la variable $price
-
-./line_order.sh "$name" "$price"
